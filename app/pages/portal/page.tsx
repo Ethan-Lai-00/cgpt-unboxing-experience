d451a8c782279a4902adef3d1916857d4c3c4a86
Ethan Lai <ethankeir00@outlook.com>
2024-11-06 00:52:14 +0800

    sample of Shadcn Sidebar

--- app/pages/portal/page.tsx
+++ app/pages/portal/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useRouter } from "next/navigation";
 import React from "react";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import AppSidebar from "../shadcnSidebar/app-sidebar";
 
 const OnboardingPortal = () => {
   const router = useRouter();
@@ -9,15 +11,30 @@ const OnboardingPortal = () => {
   };
 
   return (
-    <div className="flex h-screen justify-center items-center">
-      <div
-        className="px-6 py-4 h-fit justify-center bg-[#7D78ED] text-white font-bold cursor-pointer rounded-md"
-        onClick={handleClick}
-      >
-        Complete Onboarding
+    <SidebarProvider>
+      <AppSidebar />
+      <div className="flex h-screen justify-center items-center">
+        <div
+          className="px-6 py-4 h-fit justify-center bg-[#7D78ED] text-white font-bold cursor-pointer rounded-md"
+          onClick={handleClick}
+        >
+          Complete Onboarding
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   );
 };
 
 export default OnboardingPortal;
+
+// The code above is an example code.
+// Note that this sidebar provider and appsidebar code is suppose to wrap the structure of layout.tsx.
+// return (
+//   <SidebarProvider>
+//     <AppSidebar />
+//     <main>
+//       <SidebarTrigger />
+//       {children}
+//     </main>
+//   </SidebarProvider>
+// )
